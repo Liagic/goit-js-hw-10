62e4b9c6652498f46ccf47fc0731a34997cc7e82
liagic <leon_max@i.ua>
2026-02-17 17:17:59 +0200

    eddited task 2 and routes for svg and pages

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,42 +1,42 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-function messageSuccess(time) {
+function messageSuccess(msg) {
   iziToast.success({
     title: 'OK',
-    message: `Fulfilled promise in ${time}ms`,
+    message: msg,
     position: 'topRight',
     theme: 'dark',
     backgroundColor: '#59a10d',
   });
 }
-function messageReject(time) {
+function messageReject(msg) {
   iziToast.error({
     title: 'Error',
-    message: `Rejected promise in ${time}ms`,
+    message: msg,
     position: 'topRight',
     theme: 'dark',
     icon: '',
-    iconUrl: '../img/error.svg',
+    iconUrl: '/img/error.svg',
     backgroundColor: '#ef4040',
   });
 }
 const formDelay = document.querySelector('.form');
 formDelay.addEventListener('submit', event => {
   event.preventDefault();
-  const createMessage = (delay, onSuccess, onError) => {
-    let checkedDealy = Number.isNaN(Number(delay)) || delay <= 0 ? 0 : delay;
-    let getStatus = formDelay.querySelector(
-      'input[name="state"]:checked'
-    ).value;
-    setTimeout(() => {
-      if (getStatus === 'fulfilled') {
-        onSuccess(checkedDealy);
-      } else {
-        console.log(formDelay.elements['state']);
-        onError(checkedDealy);
-      }
-    }, checkedDealy);
-  };
-  createMessage(formDelay['delay'].value, messageSuccess, messageReject);
+  const delay = formDelay.elements['delay'].value;
+  const status = formDelay.querySelector('input[name="state"]:checked').value;
+  createMessage(delay, status).then(messageSuccess).catch(messageReject);
   formDelay.reset();
 });
+function createMessage(delay, status) {
+  let checkedDelay = Number.isNaN(Number(delay)) || delay <= 0 ? 0 : delay;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (status === 'fulfilled') {
+        resolve(`Fulfilled promise in ${checkedDelay}ms`);
+      } else {
+        reject(`Rejected promise in ${checkedDelay}ms`);
+      }
+    }, checkedDelay);
+  });
+}
